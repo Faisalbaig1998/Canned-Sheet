cbe05fb275f9b65128676dbd28eb9d26850f1364
Faisal <mirzaf317@gmail.com>
2025-03-08 16:46:18 +0530

    added more features and pages

--- sources/index.js
+++ sources/index.js
@@ -5,13 +5,32 @@ document.addEventListener("click", (e) => {
     parent = e.target.parentElement;
     text = parent.querySelector("p").innerText.trim();
     navigator.clipboard.writeText(text);
-    document.getElementById("copy-modal").style.transition = "all 0.3s";
-    document.getElementById("copy-modal").style.opacity = "1";
-    document.getElementById("copy-modal").style.top = "40%";
-    setTimeout(() => {
-      document.getElementById("copy-modal").style.opacity = "0";
-      document.getElementById("copy-modal").style.top = "50%";
-    }, 400);
+    copyfunc();
   }
   console.log("Copied content: ", text);
 });
+
+document.querySelectorAll("p").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    navigator.clipboard.writeText(e.target.innerText.trim());
+    copyfunc();
+  });
+});
+
+function copyfunc() {
+  document.getElementById("copy-modal").style.transition = "all 0.3s";
+  document.getElementById("copy-modal").style.opacity = "1";
+  document.getElementById("copy-modal").style.top = "40%";
+  setTimeout(() => {
+    document.getElementById("copy-modal").style.opacity = "0";
+    document.getElementById("copy-modal").style.top = "50%";
+    window.scrollTo(0, 0);
+  }, 400);
+}
+
+// window.onscroll = function () {
+//   const navbar = document.getElementById("navbar");
+//   if (window.scrollY > 50) {
+//     navbar.classList.add("sticky");
+//   }
+// };
